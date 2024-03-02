6379e2dc82a868c2b9a9e6c40e064a824996f986
Rohit Sonkuale <rohitsonkusale2000@gmail.com>
2024-03-02 08:03:23 +0000

    Conversion of data types

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,34 @@
+let score =" 33abc";
+let  score1 = null;
+let score2 = undefined;
+let score3 = true;
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score3);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+/*
+"33" => 33
+33abc => NaN
+true => 1, false => 0
+*/
+
+let isloggedIn = 1;
+let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(booleanIsLoggedIn);
+
+/*
+1 => true, 0 => false;
+" " => false;
+"Rohit" => true
+*/
+
+let someNumber = 33;
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof(StringNumber));
