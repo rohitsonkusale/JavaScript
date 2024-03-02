e7de7442553d0f6aa411571c563c1a673758dbdb
Rohit Sonkuale <rohitsonkusale2000@gmail.com>
2024-03-02 07:46:35 +0000

    02_dataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3); // not working beacuse we are using nodejs not browser
+
+console.log(3+3);
+
+console.log("Rohit");
+
+let name = "Rohit";
+let age = 23;
+let isloggedIn = false;
+
+//numbers => 2 to power 53
+//bigint
+//String = " ", ''
+//boolean = true/false
+//null = stadalone value
+//undefined 
+//symbol => unique
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
